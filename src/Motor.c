97710ef004ccaba26be8e5e2c2daf815256d6768
bemcgarvey <bemcgarvey@comcast.net>
2019-02-08 19:27:04 -0700

    Template project

---
+++ src/Motor.c
@@ -0,0 +1,2 @@
+#include <xc.h>
+#include "Motor.h"
---
+++ src/Motor.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Motor.h
+ * Author: bemcg
+ *
+ * Created on February 8, 2019, 4:24 PM
+ */
+
+#ifndef MOTOR_H
+#define	MOTOR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MOTOR_H */
+
